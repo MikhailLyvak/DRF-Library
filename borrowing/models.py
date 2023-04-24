3e501dba76c729811aa4fe06a3ca08c7ff3f7884
Maksym Nikityuk <maksym71417@gmail.com>
2023-04-24 14:00:23 +0300

    Models created, first commit

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from book.models import Book
+from user.models import User
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
