918e230be9beee11b7dc18fafd1b4be008953a00
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-04-26 11:33:16 +0300

    Implemented cancel functionality for payment service

--- payment/urls.py
+++ payment/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path, include
 from rest_framework import routers
-
 from payment.views import PaymentViewSet
-
 app_name = "payment"
-
 router = routers.DefaultRouter()
 router.register("payments", PaymentViewSet, basename="payments")
-
 urlpatterns = [
-
     path("", include(router.urls)),
+    path(
+        "success/",
+        PaymentViewSet.as_view({"get": "payment_success"}),
+        name="payment-success",
+    ),
+    path(
+        "cancel/",
+        PaymentViewSet.as_view({"get": "payment_cancel"}),
+        name="payment-cancel",
+    ),
 ]
-
-
-
-
-
